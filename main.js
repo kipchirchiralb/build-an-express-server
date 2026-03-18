67709fd1bf525ad2ac71356dabd5ce640d5e760c
Albert <albertkipchirchir02@gmail.com>
2026-03-18 10:02:17 +0300

    database connection

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 // main javascript file/code
 const express = require("express"); // require coomjs way of doing import
+const mysql = require("mysql2"); // import mysql2 module/package/library/framework
 
 const server = express(); // creating a http server -- get, post ,patch, put, delete
+const dbConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "tendamema",
+  database: "eldohub_library",
+});
+
 server.get("/home", (req, res) => {
   res.send(
     "Request for home recieved -- this is the response for home online -- ",
   );
 });
-
 server.get("/dashboard", (req, res) => {
   // get data from db
-  res.json({
-    message: "responging to dashboard request --- from the internet",
-    data: [23, 34, 434, 45, 54, 54, 64, 65],
+  dbConnection.query("SELECT * FROM borrowings", function (error, data) {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Databaase error occured");
+    } else {
+      res.json(data);
+    }
   });
 });
+// create a route for every table in eldohub library -- localhost:3003/books -- data all books - on the browser
+
 /// start the server -- telling node - access the network ,and waith for incoming http requests
 server.listen(3003, () => console.log("server started. running"));
 // http://localhost:3003/dashboard
